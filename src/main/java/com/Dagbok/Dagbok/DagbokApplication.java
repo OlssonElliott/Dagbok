5ea3b1923e147a054a29d3052fa6f72f0fe30423
Elliott <sendtoelle@hotmail.com>
2024-12-16 11:05:00 +0100

    added springboot

---
+++ src/main/java/com/Dagbok/Dagbok/DagbokApplication.java
@@ -0,0 +1,13 @@
+package com.Dagbok.Dagbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagbokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagbokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dagbok/Dagbok/DagbokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dagbok.Dagbok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagbokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
