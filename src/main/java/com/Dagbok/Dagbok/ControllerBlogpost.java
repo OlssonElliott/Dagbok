b23d08935d5abc567d8232f6767ccc300182904f
Elliott <sendtoelle@hotmail.com>
2024-12-16 18:26:36 +0100

    fortsätt Controller

--- src/main/java/com/Dagbok/Dagbok/ControllerBlogpost.java
+++ src/main/java/com/Dagbok/Dagbok/ControllerBlogpost.java
@@ -1,10 +1,13 @@
 package com.Dagbok.Dagbok;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerBlogpost {
@@ -13,22 +16,23 @@ public class ControllerBlogpost {
     RepositoryBlogpost repositoryBlogpost;
 
     @GetMapping("/")
-    public String getIndex() {
-        System.out.println(repositoryBlogpost.findAll());
-
+    public String getIndex(Model model) {
+        model.addAttribute("posts", repositoryBlogpost.findAll());
         return "index";
     }
 
-    @GetMapping("/add")
-    public String addPost() {
-
-        // Blogpost blogpost = new Blogpost();
-        // blogpost.setDate(LocalDate.now());
-        // blogpost.setIsHiddenInFuture(0);
-        // blogpost.setRubrik("TestRubrik");
-        // blogpost.setText("lorem ipsum deluxe times tusen");
-        // repositoryBlogpost.save(blogpost);
-
+    @PostMapping("/add")
+    public String addPost(@ModelAttribute Blogpost blogpost) {
+        // lägg till logik för hidden innan den lägger till-
+        repositoryBlogpost.save(blogpost);
         return "redirect:/";
     }
+
+    @GetMapping("/blogg/{id}")
+    public String getBlogpost(@PathVariable Integer id, Model model) {
+        Blogpost blogpost = new Blogpost();
+        blogpost = repositoryBlogpost.findById(id);
+        return new String();
+    }
+
 }
