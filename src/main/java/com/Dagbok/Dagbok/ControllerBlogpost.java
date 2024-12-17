4968ce1f9d4368dd5d7ff7ba488b6af68abce9cf
Elliott <sendtoelle@hotmail.com>
2024-12-17 13:38:05 +0100

    fixat edit

--- src/main/java/com/Dagbok/Dagbok/ControllerBlogpost.java
+++ src/main/java/com/Dagbok/Dagbok/ControllerBlogpost.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerBlogpost {
@@ -17,20 +15,19 @@ public class ControllerBlogpost {
     @Autowired
     RepositoryBlogpost repositoryBlogpost;
 
-    @GetMapping("/")
+    @GetMapping("/") // home
     public String getIndex(Model model) {
         model.addAttribute("posts", repositoryBlogpost.findAll());
         return "index";
     }
 
-    @PostMapping("/add")
+    @PostMapping("/add") // lägg till post
     public String addPost(@ModelAttribute Blogpost blogpost) {
-        // lägg till logik för hidden innan den lägger till-
         repositoryBlogpost.save(blogpost);
         return "redirect:/";
     }
 
-    @GetMapping("/blogg/{id}")
+    @GetMapping("/blogg/{id}") // visa post
     public String getPost(@PathVariable Integer id, Model model) {
         Blogpost blogpost = new Blogpost();
         blogpost = repositoryBlogpost.findById(id).orElse(null);
@@ -38,15 +35,20 @@ public class ControllerBlogpost {
         return "post";
     }
 
-    @GetMapping("/delete/{id}")
+    @GetMapping("/delete/{id}") // ta bort bost
     public String deletePost(@PathVariable Integer id) {
         repositoryBlogpost.deleteById(id);
         return "redirect:/";
     }
 
-    @PostMapping("/edit/{id}")
-    public String editPost(@PathVariable Integer id) {
-        System.out.println("länkad rätt");
+    @PostMapping("/edit/{id}") // uppdatera post
+    public String editPost(@PathVariable Integer id, @ModelAttribute Blogpost updatedBlogpost) {
+        Blogpost oldPost = repositoryBlogpost.findById(id).orElse(null);
+        if (oldPost != null) {
+            oldPost.setRubrik(updatedBlogpost.getRubrik());
+            oldPost.setText(updatedBlogpost.getText());
+            repositoryBlogpost.save(oldPost);
+        }
         return "redirect:/blogg/" + id;
     }
 
